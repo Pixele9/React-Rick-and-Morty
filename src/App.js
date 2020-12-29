69598b4eaf70cf7584b1c9051911ab7201751653
Andres Leal <pixele9@gmail.com>
2020-12-29 13:37:34 -0600

    *added* context for dark theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { useContext } from 'react';
+
 import './index.css';
 
 import Header from './components/Header';
 import Characters from './components/Characters';
 
+import ThemeContext from './context/ThemeContext';
+
 function App() {
+  const { theme } = useContext(ThemeContext);
+
+  const backgroundClass = theme ? "dark" : "light"
+
   return (
     <div className="App">
       <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,16 @@ import ThemeContext from '../context/ThemeContext';
 
 export default function Header() {
 	const [darkMode, setDarkMode] = useState(false)
-	const color = useContext(ThemeContext)
+	const { theme, setTheme } = useContext(ThemeContext)
 	
 	const handleClick = () => {
 		setDarkMode(!darkMode)
+		setTheme(!theme)
 	}
 
 	return (
 		<div className="container">
-			<h1 style={{ color }}>React Hooks</h1>
+			<h1>React Hooks</h1>
 			<button onClick={handleClick}>{ darkMode ? "Dark Mode" : "Light Mode" }</button>
 			<p>Dark Mode? {darkMode.toString()}</p>
 		</div>
